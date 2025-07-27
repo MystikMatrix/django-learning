587276b82d2a88cb57b7dd2cd3c1c886d98cbb0c
Michelle Boutilier <cybermomma74@outlook.com>
2025-07-27 12:45:31 -0300

    tutorial up to the form for the poll

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),  # /polls/
+   
     # ex: /polls/
     path("", views.index, name="index"),
     # ex: /polls/5/
