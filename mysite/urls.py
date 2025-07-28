5267fc1d72ba8b8512fcb0e79100a66e4d61b1c6
Michelle Boutilier <cybermomma74@outlook.com>
2025-07-28 09:56:08 -0300

    erasing useless comments

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),  # ✅ correct place for this line
+    path("polls/", include("polls.urls")), 
     path("admin/", admin.site.urls),
 ]
